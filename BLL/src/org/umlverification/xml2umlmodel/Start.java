71a82c4b5c3edb01a7f89296ea4725a982ab83fe
DESKTOP-KFDJ9VH\衡辰 <1532968996@qq.com>
2019-07-18 23:22:44 +0800

    no message

--- BLL/src/org/umlverification/xml2umlmodel/Start.java
+++ BLL/src/org/umlverification/xml2umlmodel/Start.java
@@ -4,7 +4,7 @@ import org.eclipse.uml2.uml.Model;
 
 public class Start {
 
-	public static XML2UMLModel xm=new XML2UMLModel("C:\\Users\\衡辰\\Desktop\\项目组\\项目实现情况\\UML-workspace\\UMLVerification\\testmodels\\test5.xml");
+	public static XML2UMLModel xm=new XML2UMLModel("C:\\Users\\衡辰\\Desktop\\项目组\\项目实现情况\\UML-workspace\\BLL\\testmodels\\test5.xml");
 
 	public static void main(String[] args)
 	{

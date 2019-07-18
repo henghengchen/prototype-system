2c3813cd524dc4155a0568779adb763813a0a4f0
DESKTOP-KFDJ9VH\衡辰 <1532968996@qq.com>
2019-07-19 00:45:44 +0800

    三层UI--BLL--DAO prototypesystemBLL为maven项目，功能与BLL一样

---
+++ prototypesystemBLL/src/main/java/org/ecsz/xml2umlmodel/Start.java
@@ -0,0 +1,13 @@
+package org.ecsz.xml2umlmodel;
+
+
+public class Start {
+	
+	public static XML2UMLModel xm=new XML2UMLModel("C:\\Users\\衡辰\\Desktop\\项目组\\项目实现情况\\UML-workspace\\prototypesystemBLL\\target\\test5.xml");
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		xm.getUMLModel();
+	}
+
+}
---
+++ prototypesystemBLL/src/main/java/org/ecsz/xml2umlmodel/package-info.java
@@ -0,0 +1,1 @@
+package org.ecsz.xml2umlmodel;
